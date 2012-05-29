26bb19aace3a9057264d5959c78a55d6844d674b
Mike O'Malley <spuriousdata@gmail.com>
2012-05-29 11:49:40 -0400

    moved signals to external module

--- janrain/views.py
+++ janrain/views.py
@@ -4,25 +4,10 @@ from django.contrib import auth
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.dispatch import Signal
 
 from janrain import api
 from janrain.models import JanrainUser
-
-class JanrainSignal(object):
-    pass
-
-pre_login         = Signal(providing_args=['request'])
-post_profile_data = Signal(providing_args=['profile_data'])
-post_authenticate = Signal(providing_args=['user', 'profile_data'])
-post_janrain_user = Signal(providing_args=['janrain_user', 'profile_data'])
-post_login        = Signal(providing_args=['user', 'profile_data'])
-pre_redirect      = Signal(providing_args=['type', 'redirect'])
-login_failure     = Signal(providing_args=['message','data'])
-
-pre_logout        = Signal(providing_args=['request'])
-logout            = Signal(providing_args=['request'])
-
+from janrain.signals import *
 
 @csrf_exempt
 def login(request):
