3d47c69d0707ddaf7839361c3cd596293fcb921c
Maxim Leonovich <lm.bsod@gmail.com>
2013-12-17 18:46:23 +0300

    replaced deprecated django.conf.urls.defaults imports to just django.conf.urls

--- janrain/urls.py
+++ janrain/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('janrain.views',
     url(r'^login/$', 'login', name='janrain_login'),
